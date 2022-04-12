e4d4473d60aa96cad02dd7b69e913d5810a06911
Ajay <ajay@Ajays-MacBook-Air.local>
2022-04-12 16:02:08 +0530

    add : favourites to frontend

--- frontend/src/models/electronApi.ts
+++ frontend/src/models/electronApi.ts
@@ -1,3 +1,7 @@
 export interface IElectronAPI {
 	notificationApi: { sendNotification: (message: string) => Promise<void> };
+	favouritesApi: {
+		get: (req: any) => Promise<any>;
+		post: (req: any) => Promise<any>;
+	};
 }
--- frontend/src/models/store.ts
+++ frontend/src/models/store.ts
@@ -5,4 +5,7 @@ export default interface IStore {
 	categoriesStore: {
 		data: any;
 	};
+	favouritesDataStore: {
+		data: any;
+	};
 }
